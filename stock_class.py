99237c4def604a08e5ed6e0e14bd3ca956607556
jkw944 <79835606+jkw944@users.noreply.github.com>
2023-06-11 12:06:57 +0200

    Delete stock_class.py

--- stock_class.py
+++
@@ -1,8 +0,0 @@
-class Stock : 
-    def __init__(self, id, symbol, name, shares, price, total):
-        self.id = id
-        self.symbol = symbol
-        self.name = name
-        self.shares = shares
-        self.price = price
-        self.total = total
