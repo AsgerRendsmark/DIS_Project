b9a6570b180d454109cc622729ceea278998f34e
jkw944 <79835606+jkw944@users.noreply.github.com>
2023-06-11 11:43:56 +0200

    Delete DIS_Project directory

--- DIS_Project/DIS_Project/MyWebApp/User.py
+++
@@ -1,9 +0,0 @@
-from flask_login import UserMixin
-
-class User(UserMixin):
-    def __init__(self, id, email, first_name, password):
-        self.id = id
-        self.email = email
-        self.first_name = first_name
-        self.password = password
-        
